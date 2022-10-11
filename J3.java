b24f9d74d24a638ecf6f69cb1795c49f1cbc50fb
serg <yyy.tre@yandex.ru>
2022-10-11 17:18:28 +0500

    задание 2 к семинару 3

--- J3.java
+++ J3.java
@@ -1,70 +1,25 @@
 package Lession.Java;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class J3 {
-
-    public static void Divine(int[] original) {
-        if (original.length <= 1)
-            return;
-        int middleIndex = original.length / 2;
-        int[] first = new int[middleIndex];
-        int[] second = new int[original.length - middleIndex];
-
-        for (int i = 0; i < middleIndex; i++)
-            first[i] = original[i];
-        for (int i = middleIndex; i < original.length; i++)
-            second[i - middleIndex] = original[i];
-
-        Divine(first);
-        Divine(second);
-        Merge(original, first, second);
-    }
-
-    public static void Merge(int[] original, int[] first, int[] second) {
-        int firstIndex = 0;
-        int secondIndex = 0;
-        int originalIndex = 0;
-        while (firstIndex < first.length && secondIndex < second.length) {
-            if (first[firstIndex] <= second[secondIndex]) {
-                original[originalIndex] = first[firstIndex];
-                firstIndex++;
-            } else {
-                original[originalIndex] = second[secondIndex];
-                secondIndex++;
-            }
-            originalIndex++;
-        }
-        while (firstIndex < first.length) {
-            original[originalIndex] = first[firstIndex];
-            firstIndex++;
-            originalIndex++;
-        }
-        while (secondIndex < second.length) {
-            original[originalIndex] = second[secondIndex];
-            secondIndex++;
-            originalIndex++;
-        }
-    }
-
-    public static void print(int[] array, String text) {
-        System.out.println(text);
-        for (int element : array)
-            System.out.print(element + " ");
-        System.out.println();
-    }
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Введите элементы массива через пробел: ");
-        String rawInput = input.nextLine();
-        String[] array = rawInput.split(" ");
-        int[] original = new int[array.length];
-        for (int i = 0; i < original.length; i++)
-            original[i] = (Integer.parseInt(array[i]));
-        print(original, "Исходный массив:");
-        Divine(original);
-        print(original, "Отсортированный массив:");
-
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-10, 10);
+            list1.add(val);
+        }
+        System.out.printf("Первоначальный список -> %s\n", list1);
+        int i = 0;
+        while (i < list1.size()) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+            } else {
+                i++;
+            }
+        }
+        System.out.printf("Список с удаленными четными числами -> %s", list1);
     }
 }

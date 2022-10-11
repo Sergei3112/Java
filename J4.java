7b81a5655ba722f52b7f901eaadc8b56496fb787
serg <yyy.tre@yandex.ru>
2022-10-11 16:33:02 +0500

    задание 2 к семинару 3

---
+++ J4.java
@@ -0,0 +1,25 @@
+package Lession.Java;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class J4 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-10, 10);
+            list1.add(val);
+        }
+        System.out.printf("Первоначальный список %s\n", list1);
+        int i = 0;
+        while (i < list1.size()) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+            } else {
+                i++;
+            }
+        }
+        System.out.printf("Список с удаленными четными числами %s", list1);
+    }
+}

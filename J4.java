b76dcd54b0948f9cd7a54323fbcadb53b448db32
serg <yyy.tre@yandex.ru>
2022-10-11 17:19:10 +0500

    задание 3 к семинару 3

--- J4.java
+++ J4.java
@@ -1,7 +1,11 @@
+
 package Lession.Java;
 
 import java.util.ArrayList;
 import java.util.Random;
+
+import static java.util.Collections.max;
+import static java.util.Collections.min;
 
 public class J4 {
     public static void main(String[] args) {
@@ -11,15 +15,26 @@ public class J4 {
             int val = rnd.nextInt(-10, 10);
             list1.add(val);
         }
-        System.out.printf("Первоначальный список %s\n", list1);
-        int i = 0;
-        while (i < list1.size()) {
-            if (list1.get(i) % 2 == 0) {
-                list1.remove(i);
-            } else {
-                i++;
+        System.out.printf("Первоначальный список -> %s\n", list1);
+        int max = max(list1);
+        int min = min(list1);
+        int maxItem = list1.get(0);
+        int minItem = list1.get(0);
+        int sumItems = 0;
+        for (int item : list1) {
+            if (item > maxItem) {
+                maxItem = item;
             }
+            if (item < minItem) {
+                minItem = item;
+            }
+            sumItems += item;
         }
-        System.out.printf("Список с удаленными четными числами %s", list1);
+        float average = (float) sumItems / list1.size();
+        System.out.printf("Максимальный элемент -> %s\n", max);
+
+        System.out.printf("Минимальный элемент -> %s\n", min);
+
+        System.out.printf("Среднее арифметическое -> %s\n", average);
     }
 }
